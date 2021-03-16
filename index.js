0e33749082300eaeb2b3a0930ee16c7b6b6240d5
softwaree40 <softwaree40@gmail.com>
2021-03-16 00:00:26 -0400

    post actiion

--- index.js
+++ index.js
@@ -33,18 +33,18 @@ app.get("/api/students/:id",(req,res)=>{
     
   
 })
-// app.post("/api/students",(req,res)=>{
-//   const student = {
+app.post("/api/students",(req,res)=>{
+  const student = {
 
-//     id: students.length + 1,
-//     firstName: req.body.firstName,
-//     lastName: req.body.lastName,
-//     age: req.body.age
-//   }
-//   students.push(student)
-//   res.send(student)
+    id: students.length + 1,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    age: req.body.age
+  }
+  students.push(student)
+  res.send(student)
 
-// })
+})
 
 // const port = process.env.PORT || 3000
 // app.listen(port, ()=> console.log(`Listening on ${port}.......`))
